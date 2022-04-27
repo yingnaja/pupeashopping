86f3bc0481f35ef040d6db85ef857c4b177a096f
yingnaja <sinyapongs@gmail.com>
2022-04-27 10:34:58 +0700

    upload 2

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class MyConstant {
+  // Gernaral
+  static String appName = 'Pupea Shopping';
+  // Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+}
